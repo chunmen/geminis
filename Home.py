ba042b33d46b8a47b248995f4046110769969d6b
chunmen <33616670+chunmen@users.noreply.github.com>
2023-12-29 11:22:09 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -16,5 +16,15 @@ st.markdown(
     Gemini demo
 '''
 )
-key = "AIzaSyDIhOkFHdO9gxEbgqIsSDEa96iXMRBJWa8"   
+st.session_state.key = "AIzaSyDIhOkFHdO9gxEbgqIsSDEa96iXMRBJWa8"
+if "key" not in st.session_state:
+    st.session_state.key = None
+
+key = st.sidebar.text_input("Your key", type="password")
+ 
+if not key:
+    st.info("Please add your key to continue.")
+    st.stop()
+else:
+    st.session_state.key=key
 
